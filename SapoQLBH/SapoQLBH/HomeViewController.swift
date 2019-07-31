2eebf9eba415808bf1ae52b481cfe0e0be2d60d8
Khánh Trắng <harry.edward456@gmail.com>
2019-07-31 23:36:35 +0700

    Add Five ViewController

---
+++ SapoQLBH/SapoQLBH/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  SapoQLBH
+//
+//  Created by Đặng Khánh  on 7/31/19.
+//  Copyright © 2019 DangKhanh. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SapoQLBH/SapoQLBH/ViewController/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  SapoQLBH
+//
+//  Created by Đặng Khánh  on 7/31/19.
+//  Copyright © 2019 DangKhanh. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
